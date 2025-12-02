d0f0985efb2b0cb19aa3446099242c75e9bf8fc7
k4144 <github.k4144@gmail.com>
2025-12-02 14:19:24 +0100

    *

---
+++ src/actions/main.py
@@ -0,0 +1,10 @@
+import os
+
+os.environ["VERSION"] = "1.0.0" 
+
+def main():
+    return (os.environ["VERSION"])
+
+if __name__ == "__main__":
+    main()
+    
---
+++ tests/test_main.py
@@ -0,0 +1,3 @@
+from actions import main
+def test_main():
+    assert main.main() == "1.0.0"    
